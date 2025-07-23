50cf6381eb198578144333000566d68920c23455
Thilo Wilts <thilowilts@googlemail.com>
2025-07-23 08:51:00 +0200

    added ios structure

---
+++ Luma/Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Thilo Wilts on 23.07.25.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Luma/LumaTests/LumaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumaTests.swift
+//  LumaTests
+//
+//  Created by Thilo Wilts on 23.07.25.
+//
+
+import Testing
+@testable import Luma
+
+struct LumaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
